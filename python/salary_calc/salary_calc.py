92f973b4e2b963c9403faca266bfa5ba352606ea
Emmanuel1907 <emm.ruffin1907@gmail.com>
2026-02-13 14:29:07 -0500

    updating

--- python/salary_calc/salary_calc.py
+++ python/salary_calc/salary_calc.py
@@ -1,4 +1,13 @@
 import pandas as pd
+def calculate_progessive_tax(income, tax_table):
+    total_tax = 0
+
+    for _, row in tax_table.itterrows():
+        lower = row["bracket_min"]
+        upper = row["bracket_max"]
+        rate = row["rate"]
+
+
 
 # Where to load data
 tax_df = pd.read_csv("tax_data.csv")
@@ -14,20 +23,26 @@ print("""
 """)
 
 
-# Get user input for job title and hourly pay
+# Get user input for job title, hourly pay, and total work hours per week
 job_title = input(f"Hey there {first_name}! What is the job title you're pursuing? ")
 hrly_pay = float(input("What is the hourly pay? $"))
+work_hours = int(input("How many hours do you work per week? "))
 print("""
 """)
 
 # Get filing status and state of residency
 filing_status = input("What is your filing status? ")
+# Makes user input lowercase
+filing_status = filing_status.strip().lower()
+
 state_code = input("What state do you reside in? ")
+# Makes user input uppercase
+state_code = state_code.strip().upper()
 print("""
 """)
 
 # Calculate weekly, bi-weekly, and annual pay
-weekly_pay = hrly_pay * 40
+weekly_pay = hrly_pay * work_hours
 biweekly_pay = weekly_pay * 2
 annual_pay = weekly_pay * 52
 
@@ -40,6 +55,9 @@ state_table = tax_df[
     (tax_df["tax_type"] == "state") &
     (tax_df["state"] == state_code)
 ]
+
+federal_tax = calculate_progressive_tax
+state_tax= 
 
 print("Calculating salary information...")
 print("""
