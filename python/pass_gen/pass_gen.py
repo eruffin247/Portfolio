0cea1fa40173ecc79f41fda5b25260ba8998ec0a
Emmanuel1907 <emm.ruffin1907@gmail.com>
2026-02-17 20:47:49 -0500

    updated

---
+++ python/pass_gen/pass_gen.py
@@ -0,0 +1,8 @@
+# This is a program to generate a random password.
+
+import random
+
+characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-="
+password = ""
+
+print(f"Your new password is: {password}")
