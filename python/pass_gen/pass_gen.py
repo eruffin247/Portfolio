42dd546834cb2ccc801f77a12703bb7b6c4daf6f
Emmanuel1907 <emm.ruffin1907@gmail.com>
2026-02-19 16:15:11 -0500

    Adding soource code

--- python/pass_gen/pass_gen.py
+++ python/pass_gen/pass_gen.py
@@ -4,5 +4,7 @@ import random
 
 characters = "abcdefghijklmnopqrstuvwxyz1234567890!@#$%^&*()_+-="
 password = ""
+for x in range(10):
+    password += random.choice(characters)
 
 print(f"Your new password is: {password}")
