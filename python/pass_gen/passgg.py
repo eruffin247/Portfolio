a02ae9079039d92364825bade35f12ddb14bceac
Emmanuel1907 <emm.ruffin1907@gmail.com>
2026-02-23 12:22:22 -0500

    Creating a gui password generator

---
+++ python/pass_gen/passgg.py
@@ -0,0 +1,4 @@
+import secrets
+
+# Tkinkter is the built-in python module for creating gui. I'm using it to create a simple user interface to display the generated password. It is not necessary for generating the password, but it makes it more user-friendly.
+import tkinter as tk
