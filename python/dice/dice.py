524b7673db94602a20101ebdbdfe8e975d61e2bc
eruffin247 <150182267+eruffin247@users.noreply.github.com>
2026-08-10 12:37:10 -0400

    Delete python/dice directory

--- python/dice/dice.py
+++
@@ -1,7 +0,0 @@
-# This is a program that simulates rolling a die.
-import random
-
-dice_one = random.randint(1, 6)
-dice_two = random.randint(1, 6)
-
-print(f"Dice one and two rolled {dice_one} and {dice_two}.")
