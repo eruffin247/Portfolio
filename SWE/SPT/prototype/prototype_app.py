b1cc123a5c9601ff154dd777521f991020f65282
Emmanuel1907 <emm.ruffin1907@gmail.com>
2026-09-15 13:07:50 -0400

    Added for loop to generate mulitple passwords at once.

--- SWE/SPT/prototype/prototype_app.py
+++ SWE/SPT/prototype/prototype_app.py
@@ -10,17 +10,25 @@ st.write("""
 This tool to for generating cryptographically secure random passwords.
 
 ## How to Use 📑
-1. Select the desired length of the password
-2. Click the "Generate Password" button
+1. Select the number of passwords you would like to generate.
+2. Select the desired length of the password.
+3. Click the "Generate Password" button.
 """)
 
 # ===Generate Password===
 st.write("## Generate Your Password")
+
+num_of_passwords = st.radio(
+    "How many passwords would you like to generate?",
+    [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    )
+
 pass_length = st.radio(
 "Length of password?",
 [12, 13, 14, 15, 16, 17, 18, 19, 20]
 )
 
 if st.button("Generate Password"):
-    password = gen_password(pass_length)
-    st.write(f"Password: {password}")
+    for i in range(num_of_passwords):
+        password = gen_password(pass_length)
+        st.write(f"Password {i + 1}: {password}")
